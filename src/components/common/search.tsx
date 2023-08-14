2db057e62ebf7212c0c311015933638e7649ce5d
vivekbhojani12 <vivek.strivedge@gmail.com>
2023-08-14 12:35:31 +0530

    digain changes

--- src/components/common/search.tsx
+++ src/components/common/search.tsx
@@ -91,7 +91,7 @@ const Search: React.FC<SearchProps> = ({
         id="search"
         {...register('searchText')}
         className={rootClassName}
-        placeholder={t('form:input-placeholder-search')}
+        placeholder={t('Search...')}
         aria-label="Search"
         autoComplete="off"
         {...rest}
