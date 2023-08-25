3f8923f9eb9b1ce116f46127d5fb4edfa8b07693
farnaz0703 <farnazk.strivedge@gmail.com>
2023-08-25 14:43:57 +0530

    changes

--- src/components/group/group-validation-schema.ts
+++ src/components/group/group-validation-schema.ts
@@ -1,7 +1,7 @@
 import * as yup from 'yup';
 export const typeValidationSchema = yup.object().shape({
   name: yup.string().required('Name required'),
-  exp_date:yup.string().required('Date is reqrror-name-required'),
+  exp_date:yup.string().required('Date is required'),
   email:yup.string().required('Email is required'),
   no_id: yup
   .mixed() // Use .mixed() to allow custom validations
