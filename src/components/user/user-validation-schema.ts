9c669170a703a4513195c91560b635c7b25ff02e
farnaz0703 <farnazk.strivedge@gmail.com>
2023-08-25 12:06:38 +0530

    changes

--- src/components/user/user-validation-schema.ts
+++ src/components/user/user-validation-schema.ts
@@ -7,11 +7,16 @@ export const customerValidationSchema = yup.object().shape({
     .email('form:error-email-format')
     .required('form:error-email-required'),
   password: yup
-    .string()
-    .required('form:error-password-required')
-    .min(8, 'Password length should be 8 character long length') // At least 8 characters long
-    .matches(
-      /^(?=.*[A-Z])(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
-      'Invalid Password'
-    ), // Contains at least one uppercase letter and one special character
+  .string()
+  .required('form:error-password-required')
+  .min(8, 'Password length should be 8 characters long')
+  .test('uppercase', 'Password should contain at least one uppercase letter', (value: any) => {
+    return /[A-Z]/.test(value);
+  })
+  .test('specialCharacter', 'Password should contain at least one special character', (value: any) => {
+    return /[@$!%*?&]/.test(value);
+  })
+  .test('number', 'Password should contain at least one Number', (value: any) => {
+    return /[0-9]/.test(value);
+  })
 });
